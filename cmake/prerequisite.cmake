73beedc8ae3d714fd04752cccb0583dacc4f45c5
Davide Gadioli <davide.gadioli@polimi.it>
2026-03-02 09:32:14 +0100

    Rework the building system to include MPI

--- cmake/prerequisite.cmake
+++ cmake/prerequisite.cmake
@@ -1,4 +1,14 @@
 # ##############################################################################
+# ### Handle third party dependenies
+# ##############################################################################
+
+# Third-party libraries that must be manually installed
+find_package(Boost CONFIG REQUIRED COMPONENTS program_options graph fiber)
+find_package(OpenBabel3 REQUIRED)
+find_package(MPI REQUIRED C)
+
+
+# ##############################################################################
 # Microsoft GSL (Guidelines Support Library)
 # On HPC we cannot rely on FetchContent (no outbound https / broken git-remote-https),
 # so we use a preinstalled package (e.g. Spack "cppgsl") or a vendored copy.
@@ -12,3 +22,5 @@ FetchContent_Declare(
   GIT_TAG "v4.0.0"
   GIT_SHALLOW ON)
 FetchContent_MakeAvailable(GSL)
+
+
